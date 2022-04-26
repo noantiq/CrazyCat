df803d8051e2d14688e5fa5381fbd3b3ae4646f5
Timuçin Boldt <timucin.boldt@tu-dortmund.de>
2022-04-26 15:25:29 +0200

    fix input check in pause menu

--- pause.gd
+++ pause.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 func _input(event):
 	if event is InputEventKey:
-		if event.pressed and event.scancode == Input.is_action_pressed("pause"):
+		if Input.is_action_pressed("pause"):
 			get_tree().paused = not get_tree().paused
 		if get_tree().paused:
 			$Popup.popup()
