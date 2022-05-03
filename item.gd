5e85dc3ea80bbf23750dee458eecad41b252e785
Timuçin Boldt <timucin.boldt@udo.edu>
2022-05-03 12:59:00 +0200

    only kick items on keyboard input

--- item.gd
+++ item.gd
@@ -2,11 +2,23 @@ extends Area2D
 
 signal thrown_off
 
+var body_entered = false
+
 func _on_AnimationPlayer_animation_finished(anim_name):
 	emit_signal("thrown_off")
 	queue_free()
 
+func _process(delta):
+	if body_entered and Input.is_action_pressed("kick"):
+		print("kick")
+		set_collision_mask_bit(0, 0)
+		$AnimationPlayer.play("fall")
+		$CollectSound.play()
+
 func _on_item_body_entered(body):
-	set_collision_mask_bit(0, 0)
-	$AnimationPlayer.play("fall")
-	$CollectSound.play()
+	print("entered")
+	body_entered = true	
+
+
+func _on_item_body_exited(body):
+	body_entered = false
